3e36e1bf757c64fd14aa462708b64522985a9fa5
Sylaeter <43379201+Sylaeter@users.noreply.github.com>
2021-10-31 22:07:58 -0500

    tweaked state_dict parsing, added sigmoid activation function

--- Python/handwritten_neural_network.py
+++ Python/handwritten_neural_network.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import transforms, datasets
-import numpy as np
+from fxpmath import Fxp
 
 training_data = datasets.MNIST('', train=True, download=True,
                             transform=transforms.Compose([transforms.ToTensor()]))
@@ -21,26 +21,26 @@ test_set = torch.utils.data.DataLoader(test_data, batch_size=10, shuffle=False)
 class NeuralNetwork(nn.Module): 
     def __init__(self):
         super().__init__()
-        self.input_layer = nn.Linear(28*28, 64)
-        self.hidden_layer = nn.Linear(64, 64)
-        self.output_layer = nn.Linear(64, 10)
+        self.input_layer = nn.Linear(28*28, 50)
+        self.hidden_layer = nn.Linear(50, 50)
+        self.output_layer = nn.Linear(50, 10)
     
     def forward(self, x):
         x = F.relu(self.input_layer(x))
-        x = F.relu(self.hidden_layer(x))
+        x = F.sigmoid(self.hidden_layer(x))
         x = self.output_layer(x)
         return F.log_softmax(x, dim=1)
 
 def trainModel(model, optimizer, training_set):
-    for epoch in range(4): # set range for however many epochs you want to run. 
+    for epoch in range(4): 
         for data in training_set:
-            X, y = data  # X is the batch of features, y is the batch of targets.
-            model.zero_grad()  # sets gradients to 0 before loss calc.
-            output = model(X.view(-1,784))  # pass in the reshaped batch (recall they are 28x28 atm)
-            loss = F.nll_loss(output, y)  # calc and grab the loss value
-            loss.backward()  # apply this loss backwards thru the model's parameters
-            optimizer.step()  # attempt to optimize weights to account for loss/gradients
-        print(loss)  # print loss. We hope loss (a measure of wrong-ness) declines! 
+            X, y = data  
+            model.zero_grad()  
+            output = model(X.view(-1,784))  
+            loss = F.nll_loss(output, y)  
+            loss.backward()  
+            optimizer.step()  
+        print(loss)  
 
 def getModelAccuracy(model, test_set):
     correct = 0
@@ -57,24 +57,20 @@ def getModelAccuracy(model, test_set):
     return round(correct/total, 3);
 
 def getWeightsAndBias(model):
-    input_layer_wb = model.input_layer.state_dict()
-    print(input_layer_wb['weight'])
-    print(input_layer_wb['bias'])
+    parseToFixedPoint(model.state_dict())
 
-    hidden_layer_wb = model.hidden_layer.state_dict()
-    print(hidden_layer_wb['weight'])
-    print(hidden_layer_wb['bias'])
-
-    output_layer_wb = model.output_layer.state_dict()
-    print(output_layer_wb['weight'])
-    print(output_layer_wb['bias'])
-
-    np.savetxt('input_layer_weight.txt', input_layer_wb['weight'])
-    np.savetxt('input_layer_bias.txt', input_layer_wb['bias'])
-    np.savetxt('hidden_layer_weight.txt', hidden_layer_wb['weight'])
-    np.savetxt('hidden_layer_bias.txt', hidden_layer_wb['bias'])
-    np.savetxt('output_layer_weight.txt', output_layer_wb['weight'])
-    np.savetxt('output_layer_bias.txt', output_layer_wb['bias'])
+def parseToFixedPoint(state_dict):
+    for layer in state_dict:
+        with open(layer, "w") as outputFile:
+            for param in state_dict[layer]:    
+                if(param.size() != torch.Size([])):
+                    for item in param:
+                        outputFile.write(str(Fxp(item.item(), 
+                            n_int=8, n_frac=8, signed=True).raw())+ " ")
+                        outputFile.write("\n")
+                else:
+                    outputFile.write(str(Fxp(param.item(), 
+                            n_int=8, n_frac=8, signed=True).raw())+ "\n")
 
 model = NeuralNetwork()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
