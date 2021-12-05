f07c4edc76f5e7bd96c7cf096dd922d839e4b9bb
Slayter Teal <slayterteal@gmail.com>
2021-12-04 20:27:15 -0600

    project reference problems

--- Verilog/memory_tb.v
+++ Verilog/memory_tb.v
@@ -1,16 +1,17 @@
-
-// I'm using a handmade file to test this. 
+ 
 module memory_tb;
 
 reg [31:0] read_address;
-wire [31:0] data;
+wire signed [31:0] data;
 
-memory #("mem_test.txt", 5) m (.read_add(read_address), .write_out(data));
+memory #(.file("output_layer_bias.mem"), .num_of_inputs(10)) m 
+    (.r_add(read_address), 
+    .w_out(data));
 
 initial begin
-read_address = 5'b00001;
-#10
-read_address = 5'b00010;
+read_address = 32'h00000001;
+#100
+read_address = 32'h00000002;
 
 end
 
